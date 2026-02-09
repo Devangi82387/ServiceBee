30844fd48150a5ab78743ff5ae57376414110198
Devangi82387 <heydevangi82@gmail.com>
2026-02-09 23:24:19 +0530

    minor change done

--- backend/server.js
+++ backend/server.js
@@ -27,12 +27,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://service-bee.vercel.app"
+  ],
+  credentials: true
+}));
 
 // Routes
 app.use("/customer", customerRoutes);
